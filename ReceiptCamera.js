3cf7c5f540f0f1033ffc7779fc9d428f666dd582
Reese Eifler <reeseeifler@Reeses-MacBook-Pro.local>
2018-06-21 13:55:51 -0400

    fix: UNSAFE applied to componentWillMount in Camera, homepage text updated, extra Camera component file removed

--- ReceiptCamera.js
+++ ReceiptCamera.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera, Permissions } from 'expo';
+import React from 'react'
+import { Text, View, TouchableOpacity } from 'react-native'
+import { Camera, Permissions } from 'expo'
 
 export default class ReceiptCamera extends React.Component {
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
-  };
+  }
 
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
+  async UNSAFE_componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA)
+    this.setState({ hasCameraPermission: status === 'granted' })
   }
 
   render() {
-    const { hasCameraPermission } = this.state;
+    const { hasCameraPermission } = this.state
     if (hasCameraPermission === null) {
-      return <View />;
+      return <View />
     } else if (hasCameraPermission === false) {
-      return <Text>No access to camera</Text>;
+      return <Text>No access to camera</Text>
     } else {
       return (
         <View style={{ flex: 1 }}>
@@ -28,7 +28,8 @@ export default class ReceiptCamera extends React.Component {
                 flex: 1,
                 backgroundColor: 'transparent',
                 flexDirection: 'row',
-              }}>
+              }}
+            >
               <TouchableOpacity
                 style={{
                   flex: 0.1,
@@ -37,20 +38,24 @@ export default class ReceiptCamera extends React.Component {
                 }}
                 onPress={() => {
                   this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back,
+                  })
+                }}
+              >
                 <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Flip{' '}
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}
+                >
+                  {' '}
+                  Flip{' '}
                 </Text>
               </TouchableOpacity>
             </View>
           </Camera>
         </View>
-      );
+      )
     }
   }
 }
