a13dbcd17984dd2f6b75c2c4194db827e62af298
Matthew Contract <mcontract27@gmail.com>
2018-06-21 12:48:12 -0400

    Placeholder Home screen and navigation

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Home from "./Home";
+import { createStackNavigator } from "react-navigation";
+import ReceiptCamera from "./ReceiptCamera";
+
+const Nav = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationoptions: {
+        headerTitle: "Home"
+      }
+    },
+    Camera: {
+      screen: ReceiptCamera,
+      navigationoptions: {
+        headerTitle: "Camera"
+      }
+    }
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    //Might need <Provider>
+    return <Nav />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
