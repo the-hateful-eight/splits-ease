366d7dc692cde8083f1272d34324e8d13d57d525
Reese Eifler <reeseeifler@Reeses-MacBook-Pro.local>
2018-06-25 11:33:14 -0400

    fix: merge conflicts

--- client/store/user.js
+++ client/store/user.js
@@ -1,10 +1,6 @@
 import axios from 'axios'
 
-<<<<<<< HEAD
 //Actions
-=======
-//Actions 
->>>>>>> master
 const GOT_USERS = 'GOT_USERS'
 const GOT_USER = 'GOT_USER'
 const GOT_USER_FRIENDS = 'GOT_USER_FRIENDS'
@@ -13,7 +9,6 @@ const GOT_USER_FRIEND = 'GOT_USER_FRIEND'
 //Action creators
 export const gotUserFriends = userFriends => ({
   type: GOT_USER_FRIENDS,
-<<<<<<< HEAD
   userFriends,
 })
 
@@ -23,18 +18,6 @@ export const getUserFriends = userId => dispatch =>
     .get(`api/user/${userId}/friends`)
     .then(res => dispatch(gotUserFriends(res.data)))
     .catch(err => console.log(err))
-=======
-  userFriends
-})
-
-//Thunks
-export const getUserFriends = (userId) =>
-  dispatch =>
-    axios.get(`api/user/${userId}/friends`)
-      .then(res =>
-        dispatch(gotUserFriends(res.data)))
-      .catch(err => console.log(err))
->>>>>>> master
 
 //Initial State
 const initialState = {
@@ -46,7 +29,6 @@ const initialState = {
 
 //Reducer
 export default function(state = initialState, action) {
-<<<<<<< HEAD
   switch (action.type) {
     case GOT_USERS:
       return {
@@ -57,23 +39,10 @@ export default function(state = initialState, action) {
       return {
         ...state,
         user: action.user,
-=======
-  switch(action.type) {
-    case GOT_USERS:
-      return {
-        ...state,
-        users: action.users
-      }
-    case GOT_USER: 
-      return {
-        ...state,
-        user: action.user
->>>>>>> master
       }
     case GOT_USER_FRIENDS:
       return {
         ...state,
-<<<<<<< HEAD
         userFriends: action.userFriends,
       }
     case GOT_USER_FRIEND:
@@ -83,16 +52,5 @@ export default function(state = initialState, action) {
       }
     default:
       return state
-=======
-        userFriends: action.userFriends
-      }
-    case GOT_USER_FRIEND: 
-      return {
-        ...state,
-        userFriend: action.userFriend
-      }
-    default: 
-      return state;
->>>>>>> master
   }
 }
