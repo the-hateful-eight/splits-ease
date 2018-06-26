2df3182b7edb52bd21a6abc416cf028688cb1cfa
Matthew Contract <mcontract27@gmail.com>
2018-06-26 14:24:04 -0400

    created Item reducer

---
+++ client/store/items.js
@@ -0,0 +1,52 @@
+const SET_ITEMS = "SET_ITEMS";
+const ASSOCIATE_ITEM = "ASSOCIATE_ITEM";
+const UNASSOCIATE_ITEM = "UNASSOCIATE_ITEM";
+
+const setReceiptItems = items => ({
+  type: SET_ITEMS,
+  items
+});
+
+const associateItem = (index, recipient) => ({
+  type: ASSOCIATE_ITEM,
+  index,
+  recipient
+});
+
+const unassociateItem = index => ({
+  type: UNASSOCIATE_ITEM,
+  index
+});
+
+export const setItems = items => {
+  return dispatch => {
+    setReceiptItems(items);
+  };
+};
+
+export const assignItem = (index, recipient) => {
+  return dispatch => {
+    associateItem(index, recipient);
+  };
+};
+
+export const unassignItem = index => {
+  return dispatch => {
+    unassociateItem(index);
+  };
+};
+
+export default function(state = [], action) {
+  switch (action.case) {
+    case SET_ITEMS:
+      return action.items;
+    case ASSOCIATE_ITEM:
+      state[action.index].belongsTo = action.recipient;
+      return state;
+    case UNASSOCIATE_ITEM:
+      delete state[action.index].belongsTo;
+      return state;
+    default:
+      return state;
+  }
+}
