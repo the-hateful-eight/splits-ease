15930ce1fe31f88848e94de1779ec92216959bb1
Reese Eifler <reeseeifler@Reeses-MacBook-Pro.local>
2018-06-27 11:15:16 -0400

    feature: regular logout started

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,25 +1,50 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { connect } from 'http2'
+import { logout } from '../store'
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Welcome to SPLITS/ease</Text>
-        <Button title="Take a picture" onPress={() => this.props.navigation.navigate('ReceiptCamera')} />
-        <Button title="Receipt Form" onPress={() => this.props.navigation.navigate('ReceiptForm')} />
-        <Button title="Receipt List" onPress={() => this.props.navigation.navigate('ReceiptList')} />
-      </View>
-    );
+const Home = props => {
+  const { logoutPress } = props
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  })
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to SPLITS/ease</Text>
+      <Button
+        title="Take a picture"
+        onPress={() => props.navigation.navigate('ReceiptCamera')}
+      />
+      <Button
+        title="Receipt Form"
+        onPress={() => props.navigation.navigate('ReceiptForm')}
+      />
+      <Button
+        title="Receipt List"
+        onPress={() => props.navigation.navigate('ReceiptList')}
+      />
+      <Button title="Logout" onClick={logoutPress} />
+    </View>
+  )
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    logoutPress: () => {
+      dispatch(logout()).then(() => {
+        ownProps.history.push('/')
+      })
+    }
   }
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center"
-    }
-  });
-
+export default connect(
+  null,
+  mapDispatchToProps
+)(Home)
