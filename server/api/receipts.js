6829265bfcbb823fe95a0da6b25aa84a87470c2a
Matthew Contract <mcontract27@gmail.com>
2018-06-29 13:52:12 -0400

    Twilio working

--- server/api/receipts.js
+++ server/api/receipts.js
@@ -1,30 +1,80 @@
-const router = require('express').Router()
-const { Receipt } = require('../db')
-const vision = require('@google-cloud/vision')
+const router = require("express").Router();
+const { Receipt } = require("../db");
+const fs = require("fs");
 
-const client = new vision.ImageAnnotatorClient()
+const twilio = require("twilio");
+const twilioClient = new twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
-const fs = require('fs')
+const vision = require("@google-cloud/vision");
+const client = new vision.ImageAnnotatorClient();
 
-module.exports = router
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const receipts = await Receipt.findAll()
-    res.json(receipts)
+    const receipts = await Receipt.findAll();
+    res.json(receipts);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-router.post('/', async (req, res, next) => {
-  const buffer = Buffer.from(req.body.image, 'base64')
+router.post("/", async (req, res, next) => {
+  const buffer = Buffer.from(req.body.image, "base64");
   try {
-    const parsed = await client.documentTextDetection(buffer)
-    fs.writeFile("bk.txt", JSON.stringify(parsed[0]), 'utf8', (err) => console.error(err))
-    res.json(parsed[0])
+    const parsed = await client.documentTextDetection(buffer);
+    fs.writeFile("bk.txt", JSON.stringify(parsed[0]), "utf8", err =>
+      console.error(err)
+    );
+    res.json(parsed[0]);
   } catch (err) {
-    console.error(err)
-    next()
+    console.error(err);
+    next();
   }
-})
+});
+
+router.post("/send", async (req, res, next) => {
+  const items = req.body.items;
+  console.log(items)
+  let recipients = {};
+  items.map(item => {
+    if (item.belongsTo) {
+      let recipient = JSON.stringify(item.belongsTo);
+      recipients[recipient] ? recipients[recipient].push(item) : recipients[recipient] = [item];
+    }
+  });
+  try {
+    Object.keys(recipients).map(recipient => {
+      let friend = JSON.parse(recipient);
+      let list = recipients[recipient];
+      let total = list.reduce((acc, item) => acc + +item.price, 0);
+      let message = "";
+      list.forEach(item => {
+        message += `${item.item}: ${item.price}\n`;
+      });
+      message += `Total: ${total}`;
+      if (friend.email) {
+        //do email stuff
+        console.log(friend.email)
+      }
+      if (friend.phone) {
+        let number = friend.phone.replace(/\(\)-\s/g, "");
+        if (!number.startsWith("+1")) number = "+1" + number;
+        twilioClient.messages
+          .create({
+            body: message,
+            to: number,
+            from: process.env.TWILIO_NUMBER
+          })
+          .then(messageRes => console.log(messageRes.sid));
+      }
+    });
+    res.send()
+  } catch (err) {
+    console.error(err);
+    next();
+  }
+});
